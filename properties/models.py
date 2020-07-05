2efbc905a998c14f45548b5957189f9276eefeb7
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-07-05 08:16:23 +0200

    Office model added. Address added to property model

--- properties/models.py
+++ properties/models.py
@@ -7,10 +7,17 @@ class Property(models.Model):
 	bathrooms = models.IntegerField(default=0)
 	description = models.TextField(blank=False)
 	pub_date = models.DateTimeField('date published')
+	address_number = models.PositiveSmallIntegerField(default=0)
+	address_street = models.CharField(max_length=150)
+	address_city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True)
 
 class Agent(models.Model):
 	agent_name = models.CharField(max_length=150)
 	email = models.EmailField()
 	property = models.ManyToManyField(Property)
 	# Set null=True because some agents might be freelance AND if we delete the office, the agent is still employed
-	office = models.ForeignKey(Office, on_delete=models.SET_NULL, null=True) 
+	office = models.ForeignKey(Office, on_delete=models.SET_NULL, null=True)
+
+class Office(models.Model):
+	office_name = models.CharField(max_length=150)
+	address = models.TextField(blank=False)
