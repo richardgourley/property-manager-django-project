3f10484e9e2c42e84b7b06b688242f0a52f7f195
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-07-05 08:07:10 +0200

    Property model added with manytomany and foreign key relationships

--- properties/models.py
+++ properties/models.py
@@ -7,3 +7,10 @@ class Property(models.Model):
 	bathrooms = models.IntegerField(default=0)
 	description = models.TextField(blank=False)
 	pub_date = models.DateTimeField('date published')
+
+class Agent(models.Model):
+	agent_name = models.CharField(max_length=150)
+	email = models.EmailField()
+	property = models.ManyToManyField(Property)
+	# Set null=True because some agents might be freelance AND if we delete the office, the agent is still employed
+	office = models.ForeignKey(Office, on_delete=models.SET_NULL, null=True) 
