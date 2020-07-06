977a4d1d0b7c35397dadbd5a70239425eadb214c
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-07-06 12:59:12 +0200

    __str__ added to Property model

--- properties/models.py
+++ properties/models.py
@@ -9,6 +9,9 @@ class City(models.Model):
 	city_name = models.CharField(max_length=150)
 
 class Property(models.Model):
+	class Meta:
+		verbose_name = 'property'
+		verbose_name_plural = 'properties'
 	property_name = models.CharField(max_length=150)
 	bedrooms = models.PositiveSmallIntegerField(default=0)
 	bathrooms = models.PositiveSmallIntegerField(default=0)
@@ -18,10 +21,15 @@ class Property(models.Model):
 	street_address = models.CharField(max_length=150)
 	city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True)
 
+	def __str__(self):
+		return self.property_name + '. CITY: ' + self.city.city_name
+
 class Agent(models.Model):
 	agent_name = models.CharField(max_length=150)
 	email = models.EmailField()
 	property = models.ManyToManyField(Property)
 	# Set null=True because some agents might be freelance AND if we delete the office, the agent is still employed
 	office = models.ForeignKey(Office, on_delete=models.SET_NULL, null=True)
+
+
 
