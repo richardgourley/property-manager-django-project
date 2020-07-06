fe2c1ab66bc709914d398d521cb27ae43cd87a43
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-07-06 20:53:29 +0200

    display_agent added to Property for displaying agent in admin

--- properties/models.py
+++ properties/models.py
@@ -2,37 +2,43 @@ from django.db import models
 
 # Create your models here.
 class Office(models.Model):
-	office_name = models.CharField(max_length=150)
-	address = models.TextField(blank=False)
+    office_name = models.CharField(max_length=150)
+    address = models.TextField(blank=False)
 
 class City(models.Model):
-	city_name = models.CharField(max_length=150)
+    city_name = models.CharField(max_length=150)
 
-	def __str__(self):
-		return self.city_name
+    def __str__(self):
+        return self.city_name
 
 class Property(models.Model):
-	class Meta:
-		verbose_name = 'property'
-		verbose_name_plural = 'properties'
-	property_name = models.CharField(max_length=150)
-	bedrooms = models.PositiveSmallIntegerField(default=0)
-	bathrooms = models.PositiveSmallIntegerField(default=0)
-	description = models.TextField(blank=False)
-	pub_date = models.DateTimeField('date published')
-	street_number = models.PositiveSmallIntegerField(default=0)
-	street_address = models.CharField(max_length=150)
-	city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True)
+    class Meta:
+        verbose_name = 'property'
+        verbose_name_plural = 'properties'
+    property_name = models.CharField(max_length=150)
+    bedrooms = models.PositiveSmallIntegerField(default=0)
+    bathrooms = models.PositiveSmallIntegerField(default=0)
+    description = models.TextField(blank=False)
+    pub_date = models.DateTimeField('date published')
+    street_number = models.PositiveSmallIntegerField(default=0)
+    street_address = models.CharField(max_length=150)
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True)
 
-	def __str__(self):
-		return self.property_name + '. CITY: ' + self.city.city_name
+    def __str__(self):
+        return self.property_name + '. CITY: ' + self.city.city_name\
+
+    def display_agent(self):
+        #Create a string for the Agent. Required for displaying agent in property list_detail.
+        agents = Agent.objects.all()
+        return ', '.join(agent.agent_name for agent in agents)
+
 
 class Agent(models.Model):
-	agent_name = models.CharField(max_length=150)
-	email = models.EmailField()
-	property = models.ManyToManyField(Property)
-	# Set null=True because some agents might be freelance AND if we delete the office, the agent is still employed
-	office = models.ForeignKey(Office, on_delete=models.SET_NULL, null=True)
+    agent_name = models.CharField(max_length=150)
+    email = models.EmailField()
+    property = models.ManyToManyField(Property)
+    # Set null=True because some agents might be freelance AND if we delete the office, the agent is still employed
+    office = models.ForeignKey(Office, on_delete=models.SET_NULL, null=True)
 
 
 
