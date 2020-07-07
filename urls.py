1db3ccc18d3dffde03d609c2c6ae655b4ffac8b1
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-07-07 20:05:59 +0200

    urls.py deleted - now in main project folder

--- urls.py
+++
@@ -1,8 +0,0 @@
-# MAIN URL FILE - PROPERTIES URLS ADDED
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('properties/', include('properties.urls')),
-    path('admin/', admin.site.urls),
-]
