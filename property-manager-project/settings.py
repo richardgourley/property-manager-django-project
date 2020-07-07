9b4992d007a5fe2ed64b8a86887a6fe0f2626f86
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-07-07 20:08:40 +0200

    Settings.py file added with only required INSTALLED APPS and DB examples setup.

---
+++ property-manager-project/settings.py
@@ -0,0 +1,28 @@
+# Only included the parts you would need to modify
+
+# Add the 'properties' app for our properties models - adding 'PropertiesConfig' from properties/apps
+INSTALLED_APPS = [
+    'properties.apps.PropertiesConfig',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+# An example of modifying the database setup - we used MySQL for this projects\
+# We set options to allow INNODB as the default storage engine - INNODB allows foreign keys
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lessonvocabularyapp',
+        'USERNAME':'username',
+        'PASSWORD':'password',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
+        'OPTIONS':{
+            'init_command':'SET default_storage_engine=INNODB'
+        }
+    }
+}
