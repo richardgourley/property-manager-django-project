efa968497e7ada8b524eb2ace705799ee586b9f1
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-07-07 20:05:29 +0200

    Urls.py created in main project folder.

---
+++ property-manager-project/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('properties/', include('properties.urls')),
+    path('admin/', admin.site.urls),
+]
