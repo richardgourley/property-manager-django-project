7df1fb518e12d027cedda1d109aa59d895e665ff
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-07-08 19:56:40 +0200

    PropertyDetailView added

--- properties/views.py
+++ properties/views.py
@@ -5,7 +5,16 @@ from .models import Property, Office
 # Create your views here.
 class IndexView(generic.ListView):
 	template_name = 'properties/index.html'
-	context_object_name = 'latest_properties'
+	context_object_name = 'properties_and_offices'
 
 	def get_queryset(self):
-		return Property.objects.all().order_by('-pub_date')[:5]
+		properties = Property.objects.all().order_by('-pub_date')[:5]
+		offices = Office.objects.all()
+		return {'properties':properties, 'offices':offices}
+
+class PropertyDetailView(generic.DetailView):
+	model = Property
+	template_name = 'properties/property-detail.html'
+
+	def get_queryset(self):
+		return Property.objects.all()
