d1388d032ea013c5cf77f9e4f9e74fe0e633e579
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-07-08 19:57:33 +0200

    Url with pk added for PropertyDetailView

--- properties/urls.py
+++ properties/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, PropertyDetailView
 
 app_name = 'properties'
 urlpatterns = [
-    path('', IndexView.as_view(), name="index")
+    path('', IndexView.as_view(), name="index"),
+    path('<int:pk>', PropertyDetailView.as_view(), name="property_display")
 ]
