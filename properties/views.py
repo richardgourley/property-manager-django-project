9a223f01f5a9c61941b82c9db7ad0bc0eab6258a
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-07-09 19:23:31 +0200

    advanced_property_search view added - with form

--- properties/views.py
+++ properties/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Property, City,Office, Agent
-from .forms import QuickPropertySearchForm
+from .forms import QuickPropertySearchForm, AdvancedPropertySearchForm
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
@@ -43,3 +44,22 @@ class AgentsView(generic.ListView):
 
     def get_queryset(self):
         return Agent.objects.filter().order_by('office')
+
+def advanced_property_search(request):
+    if request.method == 'POST':
+        form = AdvancedPropertySearchForm(request.POST)
+        if form.is_valid():
+            city_name = form.cleaned_data['city']
+            max_price = form.cleaned_data['max_price']
+            min_bedrooms = form.cleaned_data['min_bedrooms']
+            search_results = Property.objects.filter(
+                Q(price__lte=max_price)
+                & Q(city__city_name=city_name)
+                & Q(bedrooms__gte=min_bedrooms)
+            )
+            message = "We found 1 matching result" if len(search_results) == 1 else "We found {} matching results.".format(len(search_results))
+            return render(request, 'properties/advanced-property-search.html', {'search_results':search_results, 'message':message, 'form':form})
+    else:
+        form = AdvancedPropertySearchForm()
+
+    return render(request, 'properties/advanced-property-search.html', {'form':form})
