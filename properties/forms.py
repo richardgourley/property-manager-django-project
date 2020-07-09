fbbc78183199ef92434d2a8a2e5668c322ed6bf6
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-07-09 19:25:23 +0200

    min_bedrooms added to form AdvancedPropertySearchForm

--- properties/forms.py
+++ properties/forms.py
@@ -6,12 +6,17 @@ class QuickPropertySearchForm(forms.Form):
 
 class AdvancedPropertySearchForm(forms.Form):
 	city = forms.ModelChoiceField(queryset=City.objects.all(), empty_label="SELECT A CITY")
-	max_price = forms.ChoiceField([
-		('1400','1400'),
+	max_price = forms.ChoiceField(choices=[
 		('1300','1300'),
 		('1200','1200'),
 		('1100','1100'),
 		('1000','1000'),
 		('900','900'),
 		('800','800'),
-		])
+		('700','700'),
+	])
+	min_bedrooms = forms.ChoiceField(choices=[
+        ('1','1'),
+        ('2','2'),
+        ('3','3'),
+	])
