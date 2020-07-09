c0f51b8eb618066f8ce86ce394f8b282fc7e4929
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-07-09 16:15:54 +0200

    'locations' and 'meet-the-team' added to urls

--- properties/urls.py
+++ properties/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import index, PropertyDetailView, quick_property_search
+from .views import index, PropertyDetailView, quick_property_search, LocationsView, AgentsView
 
 app_name = 'properties'
 urlpatterns = [
     path('', index, name="index"),
     path('<int:pk>', PropertyDetailView.as_view(), name="property_display"),
-    path('quickSearch', quick_property_search, name="quick_property_search")
+    path('quickSearch', quick_property_search, name="quick_property_search"),
+    path('locations', LocationsView.as_view(), name="locations"),
+    path('meet-the-team', AgentsView.as_view(), name="agents")
 ]
 
