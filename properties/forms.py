d8b413058f3c7e52b8146cad95fd544c9a2d1c16
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-07-09 16:30:43 +0200

    AdvancedSearchForm added

--- properties/forms.py
+++ properties/forms.py
@@ -3,3 +3,15 @@ from .models import City
 
 class QuickPropertySearchForm(forms.Form):
 	city = forms.ModelChoiceField(queryset=City.objects.all(), empty_label="SELECT A CITY")
+
+class AdvancedSearchForm(forms.Form):
+	city = forms.ModelChoiceField(queryset=City.objects.all(), empty_label="SELECT A CITY")
+	max_price = forms.ChoiceField([
+		('1400','1400'),
+		('1300','1300'),
+		('1200','1200'),
+		('1100','1100'),
+		('1000','1000'),
+		('900','900'),
+		('800','800'),
+		])
