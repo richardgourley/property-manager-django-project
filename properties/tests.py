ebc8a65974356b53490b1947079a0705fe8a5a17
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-07-11 15:50:13 +0200

    Deleting property doesn't delete city test added

--- properties/tests.py
+++ properties/tests.py
@@ -7,17 +7,17 @@ from django.utils import timezone
 from .models import Office, City, Property, Agent
 
 # Functions that create object instances for testing
-def create_office():
-	return Office.objects.create(office_name = office_name, address = address)
+def create_office(office_name, address):
+    return Office.objects.create(office_name = office_name, address = address)
 
 def create_city(city_name):
-	return City.objects.create(city_name = city_name)
+    return City.objects.create(city_name = city_name)
 
-def create_agent(agent_name, email, office):
-	return Agent.objects.create(agent_name = agent_name, email = email, office = office)
+def create_agent(agent_name, email, property, office):
+    return Agent.objects.create(agent_name = agent_name, email = email, office = office)
 
 def create_property(property_name,bedrooms,bathrooms,description,pub_date,street_number,street_address,city,price):
-	return Property.objects.create(
+    return Property.objects.create(
         property_name = property_name,
         bedrooms = bedrooms,
         bathrooms = bathrooms,
@@ -27,7 +27,7 @@ def create_property(property_name,bedrooms,bathrooms,description,pub_date,street
         street_address = street_address,
         city = city,
         price = price    
-	)
+    )
 
 # Classes that test models
 class ModelTests(TestCase):
@@ -41,6 +41,18 @@ class ModelTests(TestCase):
     def test_city_name_is_string(self):
         city = create_city("Berlin")
         self.assertEqual(type(city.city_name), str)
+
+    def test_deleting_property_doesnt_delete_city(self):
+        city1 = create_city("Berlin")
+        property1 = create_property(
+            "Lovely new flat",3,2,"Best flat in the city", timezone.now(), 5, "Main Street", city1, 800
+        )
+        property2 = create_property(
+            "Another lovely flat",2,2,"Second best flat in the city", timezone.now(), 57, "Main Street", city1, 900
+        )
+        property1.delete()
+        property2.delete()
+        self.assertEqual(city1.city_name, "Berlin")
 
 
 
