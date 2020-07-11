45743e4e11c7df917972e21840ad6ad0fe460fa1
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-07-11 13:25:11 +0200

    Object creator functions added

--- properties/tests.py
+++ properties/tests.py
@@ -1,9 +1,34 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
 
-from .models import Property
+import datetime
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Office, City, Property, Agent
 
-class IndexTests(TestCase):
-	pass
+# Functions that create object instances for testing
+def create_office():
+	return Office.objects.create(office_name = office_name, address = address)
+
+def create_city(city_name):
+	return City.objects.create(city_name = city_name)
+
+def create_agent(agent_name, email, office):
+	return Agent.objects.create(agent_name = agent_name, email = email, office = office)
+
+def create_property(property_name,bedrooms,bathrooms,description,pub_date,street_number,street_address,city,price):
+	return Property.objects.create(
+        property_name = property_name,
+        bedrooms = bedrooms,
+        bathrooms = bathrooms,
+        description = description,
+        pub_date = pub_date,
+        street_number = street_number,
+        street_address = street_address,
+        city = city,
+        price = price    
+	)
+
+
+
+
