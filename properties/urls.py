981dc990d7de3c891ee1e08b415905a14b6bac91
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-07-11 22:39:11 +0200

    Url named 'property_display' changed to 'property_detail'

--- properties/urls.py
+++ properties/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-from .views import index, PropertyDetailView, quick_property_search, LocationsView, AgentsView, advanced_property_search
+from .views import index, PropertyDetailView, quick_property_search, LocationsView, AgentsView, advanced_property_search, about_us
 
 app_name = 'properties'
 urlpatterns = [
     path('', index, name="index"),
-    path('<int:pk>', PropertyDetailView.as_view(), name="property_display"),
+    path('<int:pk>', PropertyDetailView.as_view(), name="property_detail"),
     path('quickSearch', quick_property_search, name="quick_property_search"),
     path('locations', LocationsView.as_view(), name="locations"),
     path('meetTheTeam', AgentsView.as_view(), name="agents"),
-    path('advancedSearch', advanced_property_search, name="advanced_property_search")
+    path('advancedSearch', advanced_property_search, name="advanced_property_search"),
 ]
+
+
+
 
