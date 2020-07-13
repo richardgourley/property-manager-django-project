a997a35eb07f1bc174169abed31dfa9ff89d9d04
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-07-13 18:02:19 +0200

    AgentInLine - verbose names overriden for admin display

--- properties/admin.py
+++ properties/admin.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from .models import Property, Agent, Office, City
 
 # Register your models here.
-class AgentInLine(admin.StackedInline):
+class AgentInLine(admin.TabularInline):
 	model = Agent.property.through
+	'''
+    Override verbose name and verbose plural name only for the admin form to offer more instruction
+	'''
+	verbose_name = "AGENT"
+	verbose_name_plural = "AGENTS  - ASSIGN AN AGENT/ AGENTS TO THIS PROPERTY"
 
 class PropertyAdmin(admin.ModelAdmin):
 	list_display = ['property_name', 'bedrooms', 'bathrooms', 'city', 'display_agent', 'price']
+	
 	inlines = [
         AgentInLine,
 	]
@@ -18,6 +24,7 @@ admin.site.register(Property, PropertyAdmin)
 admin.site.register(Agent, AgentAdmin)
 admin.site.register(Office)
 admin.site.register(City)
+
 
 
 
