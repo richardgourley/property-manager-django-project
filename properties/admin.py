1b103e148a34773a10c33bb02062860cc1efd721
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-07-21 16:19:55 +0200

    get_agents in PropertyAdmin replaced display_agent in models.py

--- properties/admin.py
+++ properties/admin.py
@@ -3,29 +3,30 @@ from .models import Property, Agent, Office, City
 
 # Register your models here.
 class AgentInLine(admin.TabularInline):
-	model = Agent.property.through
-	'''
+    model = Agent.property.through
+    '''
     Override verbose name and verbose plural name only for the admin form to offer more instruction
-	'''
-	verbose_name = "AGENT"
-	verbose_name_plural = "AGENTS  - ASSIGN AN AGENT/ AGENTS TO THIS PROPERTY"
+    '''
+    verbose_name = "AGENT"
+    verbose_name_plural = "AGENTS  - ASSIGN AN AGENT/ AGENTS TO THIS PROPERTY"
 
 class PropertyAdmin(admin.ModelAdmin):
-	list_display = ['property_name', 'pub_date', 'bedrooms', 'bathrooms', 'city', 'display_agent', 'price']
-	
-	inlines = [
+    list_display = ['property_name', 'pub_date', 'bedrooms', 'bathrooms', 'city', 'get_agents', 'price']
+    
+    def get_agents(self, obj):
+        agents = obj.agent_set.all()
+        return (', ').join(str(agent) for agent in agents)
+
+    get_agents.short_description = "AGENT/S"
+
+    inlines = [
         AgentInLine,
-	]
+    ]
 
 class AgentAdmin(admin.ModelAdmin):
-	list_display = ['agent_name', 'email', 'office']
+    list_display = ['agent_name', 'email', 'office']
 
 admin.site.register(Property, PropertyAdmin)
 admin.site.register(Agent, AgentAdmin)
 admin.site.register(Office)
 admin.site.register(City)
-
-
-
-
-
