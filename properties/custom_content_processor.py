e0b5a9bfdc95441f19526fc657307b3558e27c14
richardgourley <codeleap@outlook.com>
2020-08-04 20:02:50 +0200

    Latest properties made available for index, quick search, advanced search.

---
+++ properties/custom_content_processor.py
@@ -0,0 +1,9 @@
+from .models import Property
+from django.utils import timezone
+
+# Gets 5 latest properties for display on index, quick search and advanced search pages.
+
+def latest_properties(request):
+	return {
+	    'latest_properties':Property.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+	}
