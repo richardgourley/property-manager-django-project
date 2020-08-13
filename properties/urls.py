85ab3d0a0726748ab73d69f0a778c1e12758ef52
richardgourley <codeleap@outlook.com>
2020-08-13 15:50:32 +0200

    Properties changed to property/slug

--- properties/urls.py
+++ properties/urls.py
@@ -4,7 +4,7 @@ from .views import index, PropertyDetailView, quick_property_search, LocationsVi
 app_name = 'properties'
 urlpatterns = [
     path('', index, name="index"),
-    path('<int:pk>', PropertyDetailView.as_view(), name="property_detail"),
+    path('property/<slug:slug>', PropertyDetailView.as_view(), name="property_detail"),
     path('quickSearch', quick_property_search, name="quick_property_search"),
     path('locations', LocationsView.as_view(), name="locations"),
     path('meetTheTeam', AgentsView.as_view(), name="agents"),
